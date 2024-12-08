1815cda55ef988712aa716c70fdd7f6917bfd955
Mario Romero Ávila <damntry@hotmail.com>
2024-12-08 15:06:09 +0100

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿
+using System.Reflection;
+
+[assembly: AssemblyTitle("Damntry Globals unity")]
+[assembly: AssemblyDescription("Utils for projects using Unity")]
+[assembly: AssemblyCompany("Damntry")]
+[assembly: AssemblyProduct("Damntry Globals unity")]
+[assembly: AssemblyCopyright("Copyright © Damntry 2024")]
+[assembly: AssemblyVersion("0.0.5")]
+[assembly: AssemblyFileVersion("0.0.5")]
+
