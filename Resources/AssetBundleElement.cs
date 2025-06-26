17c1a8f9a7b5d2b27998ed0f249a1987ff6b4130
Mario Romero Ávila <damntry@hotmail.com>
2025-06-26 22:04:46 +0200

    - Updated employee logic to add the technician employees - Updated the AssetBundleElement class to improve and expand its functionality.

--- Resources/AssetBundleElement.cs
+++ Resources/AssetBundleElement.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
 using Damntry.Utils.Logging;
 using Damntry.Utils.Reflection;
 using UnityEngine;
@@ -12,11 +15,12 @@ namespace Damntry.UtilsUnity.Resources {
 	/// </summary>
 	public class AssetBundleElement {
 
+		private static Dictionary<string, AssetBundle> bundleCacheStorage = new();
+
+
 		private readonly string assetBundlePath;
 
 		private AssetBundle loadedBundle;
-
-		private readonly Dictionary<string, UnityEngine.Object> loadedPrefabs;
 
 
 		public AssetBundleElement(Type assemblyType, string assetName) {
@@ -24,59 +28,105 @@ namespace Damntry.UtilsUnity.Resources {
 				throw new ArgumentNullException(nameof(assemblyType));
 			}
 			if (string.IsNullOrEmpty(assetName)) {
-				throw new ArgumentNullException($"The parameter {assetName} cant be null or empty.");
+				throw new ArgumentNullException($"The parameter {nameof(assetName)} cant be null or empty.");
 			}
 
 			assetBundlePath = AssemblyUtils.GetCombinedPathFromAssemblyFolder(assemblyType, assetName);
 			if (!File.Exists(assetBundlePath)) {
 				throw new FileNotFoundException(null, assetBundlePath);
 			}
+		}
 
-			loadedPrefabs = new();
+		public AssetBundleElement(string assetFullPath) {
+			if (string.IsNullOrEmpty(assetFullPath)) {
+				throw new ArgumentNullException($"The parameter {nameof(assetFullPath)} cant be null or empty.");
+			}
+
+			if (!File.Exists(assetFullPath)) {
+				throw new FileNotFoundException(null, assetFullPath);
+			}
+			assetBundlePath = assetFullPath;
 		}
 
 		/// <summary>
-		/// Load the bundle from the constructor given path. Useful when you want
-		/// to load it earlier so when instanced it avoids the loading cost.
+		/// Load the bundle from the constructor given path. Useful when
+		/// you want to load it earlier and avoid that cost later on.
 		/// </summary>
 		/// <returns>True if it loaded successfully, or was previously already loaded. False otherwise.</returns>
 		public bool PreloadBundle() {
 			if (loadedBundle == null) {
-				loadedBundle = AssetBundle.LoadFromFile(assetBundlePath);
-				if (loadedBundle == null) {
-					TimeLogger.Logger.LogTimeError($"Failed to load bundle from {assetBundlePath}", LogCategories.Other);
-					return false;
+				//	See if this GetAllLoadedAssetBundles can be used to find an already loaded one.
+				//AssetBundle.GetAllLoadedAssetBundles()
+				if (!bundleCacheStorage.TryGetValue(assetBundlePath, out loadedBundle) || loadedBundle == null) {
+					loadedBundle = AssetBundle.LoadFromFile(assetBundlePath);
+					if (loadedBundle == null) {
+						throw new LoadBundleFileException(assetBundlePath);
+					}
+					TimeLogger.Logger.LogTimeDebug($"Bundle loaded successfully from {assetBundlePath}"
+						, LogCategories.Loading);
+
+					bundleCacheStorage.Add(assetBundlePath, loadedBundle);
+				} else {
+					TimeLogger.Logger.LogTimeDebug($"Bundle loaded successfully from storage cache.", LogCategories.Loading);
 				}
 			}
 			return true;
 		}
-
-		/// <summary>
-		/// Loads a prefab object for later use.
-		/// </summary>
-		/// <param name="fullLoadPath">Path to the object within the .</returns>
-		private bool TryLoadPrefabObject<T>(string fullLoadPath, out T loadedObject, bool cacheObject)
-				where T : UnityEngine.Object {
-
-			loadedObject = default;
-
+		/*
+		public string[] GetAllScenePaths() {
 			if (!PreloadBundle()) {
-				return false;
+				return null;
 			}
 
-			loadedObject = loadedBundle.LoadAsset<T>(fullLoadPath);
-			if (loadedObject == null) {
-				return false;
+			return loadedBundle.GetAllScenePaths();
+		}
+		*/
+		public string[] GetAllAssetNames() {
+			if (!PreloadBundle()) {
+				return null;
 			}
 
-			if (cacheObject) {
-				loadedPrefabs.Add(fullLoadPath, loadedObject);
+			return loadedBundle.GetAllAssetNames();
+		}
+		/*
+		public string GetAllScenePathsString(string separator = "\n") {
+			string[] scenePaths = GetAllScenePaths();
+			if (scenePaths == null) {
+				return null;
 			}
-
-			return true;
+			return string.Join(separator, scenePaths);
+		}
+		*/
+		public string GetAllAssetNamesString(string separator = "\n") {
+			string[] assetNames = GetAllAssetNames();
+			if (assetNames == null) {
+				return null;
+			}
+			return string.Join(separator, assetNames);
 		}
 
-		public bool TryLoadNewPrefabInstance(string prefabName, out GameObject prefabInstance, bool cacheObject = true) {
+		public async Task<T[]> LoadAllAssetsAsync<T>() where T : UnityEngine.Object {
+
+			if (!PreloadBundle()) {
+				return null;
+			}
+
+			AssetBundleRequest assetRequest = loadedBundle.LoadAllAssetsAsync(typeof(T));
+			await assetRequest;
+
+			return assetRequest.allAssets.Cast<T>().ToArray();
+		}
+
+		public T[] LoadAllAssets<T>() where T : UnityEngine.Object {
+
+			if (!PreloadBundle()) {
+				return null;
+			}
+
+			return loadedBundle.LoadAllAssets<T>().ToArray();
+		}
+
+		public bool TryLoadNewPrefabInstance(string prefabName, out GameObject prefabInstance) {
 			prefabInstance = null;
 			string fullLoadPath = $"assets/{prefabName}.prefab";
 
@@ -88,24 +138,71 @@ namespace Damntry.UtilsUnity.Resources {
 			return true;
 		}
 
-		public bool TryLoadObject<T>(string loadPath, out T objectInstance, bool cacheObject = true)
+		/// <summary>
+		/// Loads a Unity object for later use.
+		/// </summary>
+		/// <param name="bundleInternalUnityPath">Path to the object within the bundle.</returns>
+		public bool TryLoadObject<T>(string bundleInternalUnityPath, out T objectInstance)
 				where T : UnityEngine.Object {
 
 			objectInstance = default;
-			if (!loadedPrefabs.TryGetValue(loadPath, out UnityEngine.Object unityObject)) {
-				//If not found, load it from bundle file.
-				if (!TryLoadPrefabObject(loadPath, out unityObject, cacheObject)) {
-					return false;
-				}
+			//If not found, load it from bundle file.
+			if (!TryLoadPrefabObject(bundleInternalUnityPath, out UnityEngine.Object unityObject)) {
+				return false;
 			}
 			if (unityObject is not T) {
-				TimeLogger.Logger.LogTimeError($"The object at {loadPath} is not a {typeof(T).Name} type.", 
-					LogCategories.Other);
+				TimeLogger.Logger.LogTimeError($"The object at {bundleInternalUnityPath} " +
+					$"is not a {typeof(T).Name} type.", LogCategories.Loading);
+				return false;
 			}
 
 			objectInstance = (T)unityObject;
 			return true;
 		}
 
+		/// <summary>
+		/// Loads a prefab object for later use.
+		/// </summary>
+		/// <param name="bundleInternalUnityPath">Path to the object within the bundle.</returns>
+		private bool TryLoadPrefabObject<T>(string bundleInternalUnityPath, out T loadedObject)
+				where T : UnityEngine.Object {
+
+			loadedObject = default;
+
+			if (!PreloadBundle()) {
+				return false;
+			}
+
+			loadedObject = loadedBundle.LoadAsset<T>(bundleInternalUnityPath);
+			if (loadedObject == null) {
+				TimeLogger.Logger.LogTimeDebug($"Asset loaded with path " +
+					$"\"{bundleInternalUnityPath}\" not found.", LogCategories.Loading);
+				return false;
+			}
+
+			return true;
+		}
+
+		public void UnloadBundle() {
+			if (loadedBundle == null) {
+				TimeLogger.Logger.LogTimeDebug($"No loaded bundle was " +
+					$"found while trying to unload.", LogCategories.Loading);
+				return;
+			}
+			loadedBundle.Unload(true);
+			loadedBundle = null;
+			bundleCacheStorage.Remove(assetBundlePath);
+
+
+			TimeLogger.Logger.LogTimeDebug($"Asset bundle at {assetBundlePath} " +
+				$"unloaded successfully.", LogCategories.Loading);
+		}
 	}
+
+	public class LoadBundleFileException : Exception {
+
+		public LoadBundleFileException(string assetPath)
+				: base($"Failed to load asset bundle from path: {assetPath}.") { }
+	}
+
 }
